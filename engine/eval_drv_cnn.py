1fda9e9f48170423e3cf933cfd0e82fc8e17e1c0
tpnam0901 <tpnam0901@gmail.com>
2026-04-17 17:57:19 +0900

    fix: ae and eval cnn model

--- engine/eval_drv_cnn.py
+++ engine/eval_drv_cnn.py
@@ -1,1 +1,9 @@
-from .eval_drv_lstm import EvaluateEngine
+import networks
+
+from .eval_drv_lstm import EvaluateEngine as EvaluateEngineBase
+
+
+class EvaluateEngine(EvaluateEngineBase):
+    def build_model(self):
+        """Build the model for training."""
+        return networks.CNN(self.cfg)
