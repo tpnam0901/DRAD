b43f85039707b0d04ab63e5c884773c2bf4a488e
Trần Phương Nam <tpnam0901@gmail.com>
2026-07-15 19:55:24 +0900

    fix: missing script to generate labels from battery data
    This script processes battery brand data, extracting metadata from .pkl files and writing it to CSV files with labels.

---
+++ DRAD/data/build_label.py
@@ -0,0 +1,69 @@
+import warnings
+
+# Ignore all warnings
+warnings.simplefilter("ignore")
+
+import os
+import torch
+import glob
+import csv
+from tqdm.auto import tqdm
+
+import argparse
+import logging
+
+
+def main(data_root: str):
+    brand_folders = os.listdir(data_root)
+    brand_folders = [brand for brand in brand_folders if brand.startswith("battery_brand")]
+    for index, brand in enumerate(brand_folders):
+        logging.info(f"Processing brand {index + 1}/{len(brand_folders)}: {brand}")
+        prefixs = ["data"] if brand.endswith("3") else ["train", "test"]
+        for prefix in prefixs:
+            brand_train_path = os.path.join(data_root, brand, prefix)
+            if prefix == "data":
+                prefix = "train"
+            out_path = os.path.join(data_root, brand, f"{prefix}_labels.csv")
+            logging.info(f"Output path for labels: {out_path}")
+
+            pkl_files = glob.glob(os.path.join(brand_train_path, "*.pkl"))
+            with open(out_path, "w", newline="") as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(["filename", "label", "car", "mileage", "charge_segment"])
+
+                for pkl_file in tqdm(pkl_files):
+                    metadata = torch.load(pkl_file, weights_only=False)[1]
+                    filename = os.path.basename(pkl_file)
+                    # Label is a string "10" or "00". 10 means abnormal, 00 means normal. -> extract the first character
+                    label = int(metadata["label"][0])
+                    car = metadata["car"]
+                    mileage = metadata["mileage"]
+                    charge_segment = metadata["charge_segment"]
+                    writer.writerow([filename, label, car, mileage, charge_segment])
+
+    logging.info("Label files successfully created.")
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-dr",
+        "--data_root",
+        type=str,
+        default="battery_data",
+        help="Path to the root directory of the dataset",
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = arg_parser()
+    level = logging.INFO
+    logging.basicConfig(
+        level=level,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+    )
+    logger = logging.getLogger()
+    logger.setLevel(level)
+
+    main(args.data_root)
